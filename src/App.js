7488b0dca2b96e3a00257c52461353a0de617054
Christopher Park <christopherpark88@gmail.com>
2023-08-18 05:33:36 -0400

    expanded frontend library

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import "./App.css";
 
 import React, { useState, useEffect } from "react";
 
+// Material UI Imports
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import Typography from "@mui/material/Typography";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+
 // Amplify UI Components
 import { NavBarHeader2 } from "./ui-components";
 import { NavBarHeader } from "./ui-components";
@@ -18,6 +30,20 @@ import "@aws-amplify/ui-react/styles.css";
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
+// Styles
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "75%",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 function App({ signOut, user }) {
   const [checkoutOpen, setCheckoutOpen] = useState(false);
   const [cart, setCart] = useState([]);
@@ -29,7 +55,68 @@ function App({ signOut, user }) {
     } else {
       setCheckoutLabel(checkoutLabel.slice(0, 8));
     }
+    console.log("current cart", cart);
   }, [cart]);
+
+  // Cart Component
+  function Cart({ cartItems }) {
+    return (
+      <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+        <Typography id="modal-modal-title" variant="h3" component="h2">
+          Shopping Cart
+        </Typography>
+        <br />
+        {cartItems.length ? (
+          cartItems.map((product) => {
+            return (
+              <div>
+                <Divider />
+                <ListItem alignItems="center">
+                  <ListItemText
+                    id={product.id}
+                    primary={`${product[1]}`}
+                    secondary={`${product[3]}`}
+                  />
+
+                  <Typography
+                    id="modal-modal-title"
+                    variant="h5"
+                    component="h6"
+                  >
+                    {`$${product[2]}`}
+                  </Typography>
+                  <IconButton
+                    onClick={() => {
+                      setCart(removeElement([...cartItems], product));
+                    }}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItem>
+                <Divider />
+              </div>
+            );
+          })
+        ) : (
+          <Typography variant="h6" component="h2">
+            Cart Empty
+          </Typography>
+        )}
+      </List>
+    );
+  }
+
+  // Utilities
+  let removeElement = (array, n) => {
+    let newArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] !== n) {
+        newArray.push(array[i]);
+      }
+    }
+    return newArray;
+  };
 
   return (
     <div>
@@ -40,10 +127,25 @@ function App({ signOut, user }) {
             Button39493466: { onClick: () => setCheckoutOpen(true) },
           }}
         />
-        {console.log("checkout check", checkoutOpen)}
+        <Modal
+          open={checkoutOpen}
+          onClose={() => setCheckoutOpen(false)}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Cart cartItems={cart} />
+            {/*  <Typography id="modal-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography> */}
+          </Box>
+        </Modal>
         <br />
         <ActionCardCollection
-          overrideItems={(item, index) => {
+          overrideItems={({ item, index }) => {
             return {
               overrides: {
                 Button: {
@@ -51,6 +153,7 @@ function App({ signOut, user }) {
                     setCart((oldCart) => [
                       ...oldCart,
                       [
+                        item.id,
                         item.title,
                         item.price,
                         item.description,
